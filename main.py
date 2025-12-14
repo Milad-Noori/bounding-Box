af143581a02c0aae62b86bf6507943361ea5b176
milad <seyeadmiladnouri@gmail.com>
2025-12-14 12:33:46 +0330

    blue_Box

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import pandas as pd
 import numpy as np
 import cv2
+
+img = cv2.imread('images/Screenshot 2025-12-09 173308.png')
+row,column,channel = img.shape
+pt1=(100,280)
+pt2=(370,row-1)
+color = (255,0,0)
+thickness = 1
+cv2.rectangle(img,pt1,pt2,color,thickness)
+cv2.imshow('img',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
