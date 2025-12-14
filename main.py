75d171dedfb9e84dcc83151dd1ff75a716024d76
milad <seyeadmiladnouri@gmail.com>
2025-12-14 14:40:50 +0330

    Polly-line

--- main.py
+++ main.py
@@ -36,30 +36,52 @@ import cv2
 
 
 # --------------------------------------------------------------------------------------
+#
+# img = np.zeros((500,500,3),dtype="uint8")
+# center = (250,250)
+# radius = 100
+# color = (0,0,255)
+# thickness = 1
+# cv2.circle(img, center, radius, color, thickness)
+#
+# img1 = np.zeros((500,500,3),dtype="uint8")
+# center1 = (250,250)
+# radius1 = 200
+# color1 = (255,0,0)
+# thickness1 = 1
+# cv2.circle(img1, center1, radius1, color1, thickness1)
+#
+# img2 = np.zeros((500,500,3),dtype="uint8")
+# center2 = (20,50)
+# radius2 = 50
+# color2 = (180,180,180)
+# thickness2 = 1
+# cv2.circle(img2, center2, radius2, color2, thickness2)
+# img3 = img1+img2+img
+# plt.imshow(img3)
+# plt.show()
 
-img = np.zeros((500,500,3),dtype="uint8")
-center = (250,250)
-radius = 100
+# --------------------------------------------------------------------------------------
+
+img= np.zeros((600,600,3),np.uint8)
+pt1=np.array([[100,100],[200,100],[200,200],[100,200]],np.int32)
+pt2=np.array([[300,100],[400,200],[300,200]],np.int32)
+
 color = (0,0,255)
-thickness = 1
-cv2.circle(img, center, radius, color, thickness)
+cv2.polylines(img,[pt1,pt2],isClosed=False,color = color,thickness=2)
+cv2.imshow('img',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-img1 = np.zeros((500,500,3),dtype="uint8")
-center1 = (250,250)
-radius1 = 200
-color1 = (255,0,0)
-thickness1 = 1
-cv2.circle(img1, center1, radius1, color1, thickness1)
 
-img2 = np.zeros((500,500,3),dtype="uint8")
-center2 = (20,50)
-radius2 = 50
-color2 = (180,180,180)
-thickness2 = 1
-cv2.circle(img2, center2, radius2, color2, thickness2)
 
-img3 = img1+img2+img
-plt.imshow(img3)
-plt.show()
+
+
+
+
+
+
+
+
 
 
