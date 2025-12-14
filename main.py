0036b259cf9f4b41682d0be74872c9f4aa87ad27
milad <seyeadmiladnouri@gmail.com>
2025-12-14 13:52:07 +0330

    Mask

--- main.py
+++ main.py
@@ -22,11 +22,13 @@ import cv2
 img = cv2.imread('images/Screenshot 2025-12-09 173308.png')
 row,column,channel = img.shape
 pt1=(100,280)
-pt2=(370,row-1)
+pt2=(370,row-10)
 color = (128/255,128/255,128/255 )
 thickness = 1
-img = cv2.normalize(img,None,0,1,norm_type=cv2.NORM_MINMAX ,dtype=cv2.CV_32FC3)
-cv2.rectangle(img,pt1,pt2,color,thickness)
+img =cv2.normalize(img,None,0,1,norm_type=cv2.NORM_MINMAX ,dtype=cv2.CV_32F)
+mask = np.zeros_like(img)
+cv2.rectangle(mask,pt1,pt2,color,thickness)
+img = img+ 0.2 * mask
 cv2.imshow('img',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
